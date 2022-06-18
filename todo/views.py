22ebce555208c7bcf7174cfa6e08f39f92eea620
entropyAMNESTY <entropy.random.squared@gmail.com>
2022-06-18 10:42:38 +0200

    implemented authentication

--- todo/views.py
+++ todo/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate 
 
 # Create your views here.
 
@@ -33,17 +33,12 @@ def loginuser(request):
         return render(request, 'todo/loginuser.html', {'form':AuthenticationForm()})
     # Create new user
     else:
-        if request.POST['password1'] == request.POST['password2']:
-            try:
-                user =  User.objects.create_user(request.POST['username'], password=request.POST['password1'] )
-                user.save()
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html', {'form':AuthenticationForm(), 'error':'Username and password didnt match'})
+        else:
                 login(request, user)
                 return redirect('currenttodos')
-            except IntegrityError:
-                return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error': 'Username already been taken. Pleasue choose a new username.'})
-
-        else:
-            return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error': 'Passwords did not match'})    
 
 def logoutuser(request):
     #Only logout USER if it is a POST-request. !BrowserLoading in advance restriction 
