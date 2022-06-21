13ad9d72380603e3332646a57445f67c716397ae
entropyAMNESTY <entropy.random.squared@gmail.com>
2022-06-21 16:20:49 +0200

    implemented user authentication via generated tokenAuthentication

--- api/views.py
+++ api/views.py
@@ -2,6 +2,25 @@ from rest_framework import generics, permissions
 from .serializers import TodoSerializer, TodoCompleteSerializer
 from todo.models import Todo
 from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http import JsonResponse
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)
+            user =  User.objects.create_user(data['username'], password=data['password'])
+            user.save()
+            #Generating Tokens
+            token = Token.objects.create(user=user)
+            return JsonResponse({'token':str(token)}, status=201)
+        except IntegrityError:
+            return JsonResponse({'error':'That username has already been taken. Please choose a new username.'}, status=400)
 
 
 class TodoCompletedList(generics.ListAPIView):
@@ -44,6 +63,6 @@ class TodoComplete(generics.UpdateAPIView):
         user = self.request.user
         return Todo.objects.filter(user=user)
 
-    def perform_update(sself, serializer):
+    def perform_update(self, serializer):
         serializer.instance.datecompleted = timezone.now()
         serializer.save()
